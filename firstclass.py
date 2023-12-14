c1ecf097d965d678fc6649876149eb6553cffcf8
Hassan Raza <hr4445732@gmail.com>
2023-12-14 17:46:56 +0500

    first commit

---
+++ firstclass.py
@@ -0,0 +1,37 @@
+#multiplication
+#print(2*8)
+#division
+#print(10 / 2)
+
+#CALCULATING BMI
+#Height=1.79
+#Weight=74.2 #<-
+#bmi = Weight / Height ** 2
+#print(bmi)
+#type (bmi)
+# Create the variables monthly_savings and num_months
+
+# Create the variables monthly_savings and num_months
+
+#monthly_savings = 10
+#num_months = 4
+
+# Multiply monthly_savings and num_months
+
+#new_savings = monthly_savings*num_months
+
+# Add new_savings to your savings
+#total_savings = savings+new_savings
+
+# Print total_savings
+##print(total_savings)
+#monthly_savings = 10
+##num_months = 12
+
+#year_savings = monthly_savings * num_months
+
+#print(type(year_savings))
+#intro = "Hello!"
+#doubleintro = intro + intro
+
+#print(doubleintro)
---
+++ firstclass2.py
@@ -0,0 +1,18 @@
+# Import the math package
+import math
+print(math.pi)
+# Definition of radius
+r = 0.43
+
+# Calculate C
+radius = 0.43
+Circumference = 2 * math.pi * radius
+C= Circumference
+# Calculate A
+radius = 0.43
+Area = math.pi * radius * radius
+A= Area
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
